dfb53bedad4720b79480fcd80aa1618207f7e9ac
MykolaMak <makolamak@gmail.com>
2020-05-05 17:56:25 +0300

    Updates

---
+++ precache-manifest.fc3c96b97d0190e99bdcb0b70c986aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab727df1e96d82a1f9b11a275af4c894",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c64614be6c80ef993ad",
+    "url": "./static/css/main.6d8fcf50.chunk.css"
+  },
+  {
+    "revision": "b22e5a7f600992bf5363",
+    "url": "./static/js/2.e1899e8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e1899e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c64614be6c80ef993ad",
+    "url": "./static/js/main.e317f673.chunk.js"
+  },
+  {
+    "revision": "23032c838e70e86a4a18",
+    "url": "./static/js/runtime-main.027178ab.js"
+  }
+]);
